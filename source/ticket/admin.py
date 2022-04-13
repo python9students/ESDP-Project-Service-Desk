af4cd3e56508f634628dc2f9074b61bbd742e726
Farhatjan <faralost@gmail.com>
2022-04-13 07:52:58 +0600

    Создал суперпользователя "admin". Зарегистрировал модели в админ панели.

--- source/ticket/admin.py
+++ source/ticket/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from ticket.models import (CompanyType,
+                           ServiceObjectType,
+                           ServiceObjectModel,
+                           Country,
+                           City,
+                           Client,
+                           ServiceObject)
+
+admin.site.register(CompanyType)
+admin.site.register(ServiceObjectType)
+admin.site.register(ServiceObjectModel)
+admin.site.register(Country)
+admin.site.register(City)
+admin.site.register(Client)
+admin.site.register(ServiceObject)
