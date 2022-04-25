3595b5b08301c0dc9d64906e9e58d4ef1e6a737f
NooruzBay <nooruzbay@gmail.com>
2022-04-25 11:04:09 +0600

    В зависимости от админа или оператора показывается разная форма.

--- source/ticket/forms.py
+++ source/ticket/forms.py
@@ -48,3 +48,25 @@ class ChiefForm(forms.ModelForm):
                                              attrs={'class': 'form-control', 'type': 'datetime-local'}),
         }
         exclude = ("work_started_at", "work_finished_at", "ride_started_at", "ride_finished_at")
+
+
+class OperatorForm(forms.ModelForm):
+    description = forms.CharField(required=False, max_length=1000, widget=widgets.Textarea())
+    cancel_reason = forms.CharField(required=False, max_length=255)
+
+    class Meta:
+        model = Ticket
+        fields = ("client", "service_object", "priority", "type",
+                  "status", "service_level", "department", "recieved_at",
+                  "desired_to", "operator", "works", "problem_areas",
+                  "description", "executor", "cancel_reason",)
+
+        widgets = {
+            'recieved_at': forms.DateTimeInput(format=('%d/%m/%Y %H:%M'),
+                                               attrs={'class': 'form-control', 'type': 'datetime-local'}),
+            'desired_to': forms.DateTimeInput(format=('%d/%m/%Y %H:%M'),
+                                              attrs={'class': 'form-control', 'type': 'datetime-local'}),
+            'closed_at': forms.DateTimeInput(format=('%d/%m/%Y %H:%M'),
+                                             attrs={'class': 'form-control', 'type': 'datetime-local'}),
+        }
+        exclude = ("driver", "executor", "work_started_at", "work_finished_at", "ride_started_at", "ride_finished_at",)
--- source/ticket/views/chief.py
+++ source/ticket/views/chief.py
@@ -1,5 +1,9 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.models import Permission, Group
 from django.views.generic import CreateView, ListView, DetailView, UpdateView
-from ticket.forms import ChiefForm
+
+from accounts.views import User
+from ticket.forms import ChiefForm, OperatorForm
 from ticket.models import Ticket
 from django.urls import reverse
 
@@ -10,10 +14,22 @@ class ChiefTicketListView(ListView):
     context_object_name = 'chief_tickets'
 
 
-class ChiefTicketCreateView(CreateView):
+class ChiefTicketCreateView(LoginRequiredMixin, CreateView):
     model = Ticket
     template_name = 'chief/create.html'
-    form_class = ChiefForm
+
+    def get_form(self, form_class=None):
+        user = User.objects.get(pk=self.request.user.id)
+        group = Group.objects.get(name='operators')
+        if self.request.user.is_superuser:
+            self.form_class = ChiefForm
+        elif group in self.request.user.groups.all():
+            self.form_class = OperatorForm
+        elif self.request.user.is_staff:
+            self.form_class = ChiefForm
+        else:
+            self.form_class = OperatorForm
+        return super().get_form()
 
     def get_success_url(self):
         return reverse("ticket:chief_ticket_detail", kwargs={"pk": self.object.pk})
@@ -29,7 +45,19 @@ class ChiefTicketUpdateView(UpdateView):
     model = Ticket
     template_name = 'chief/update.html'
     context_object_name = 'chief_ticket'
-    form_class = ChiefForm
+
+    def get_form(self, form_class=None):
+        user = User.objects.get(pk=self.request.user.id)
+        group = Group.objects.get(name='operators')
+        if self.request.user.is_superuser:
+            self.form_class = ChiefForm
+        elif group in self.request.user.groups.all():
+            self.form_class = OperatorForm
+        elif self.request.user.is_staff:
+            self.form_class = ChiefForm
+        else:
+            self.form_class = OperatorForm
+        return super().get_form()
 
     def get_success_url(self):
         return reverse("ticket:chief_ticket_detail", kwargs={"pk": self.object.pk})
