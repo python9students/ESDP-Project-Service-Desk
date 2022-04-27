56195a395aa0d084e5016c3dfb96eb98a82cdafc
NooruzBay <nooruzbay@gmail.com>
2022-04-27 21:51:47 +0600

    Реализовал закрытие заявки: Отображается дата закрытия заявки при детальном просмотре, при update и create исключена. Добавил кнопку "закрыть заявку" в редактировании и при закрытии статус автоматически меняется на "заверщенный" и автоматически устанавливается дата закрытия.

--- source/ticket/views/ticket.py
+++ source/ticket/views/ticket.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.auth.models import Permission, Group
+from django.http import HttpResponseRedirect
+from django.utils import timezone
 from django.views.generic import CreateView, ListView, DetailView, UpdateView
 
 from ticket.forms import ChiefForm, OperatorForm, EngineerForm
-from ticket.models import Ticket
+from ticket.models import Ticket, TicketStatus
 from django.urls import reverse
+from datetime import datetime
 
 
 class TicketListView(ListView):
@@ -49,7 +52,6 @@ class TicketUpdateView(UpdateView):
     def get_form(self, form_class=None):
         user = self.request.user
         group = self.request.user.groups.get(user=user)
-        print(group)
         operators = Group.objects.filter(name='operators')
         chiefs = Group.objects.filter(name='chiefs')
         engineers = Group.objects.filter(name='engineers')
@@ -59,7 +61,21 @@ class TicketUpdateView(UpdateView):
             self.form_class = OperatorForm
         elif group in engineers:
             self.form_class = EngineerForm
+        else:
+            self.form_class = ChiefForm
         return super().get_form()
+
+    def form_valid(self, form):
+        status = TicketStatus.objects.get(name="Завершенный")
+        if 'close_ticket' in self.request.POST:
+            self.object = form.save(commit=False)
+            self.object.status = status
+            self.object.closed_at = timezone.now()
+            self.object.save()
+            return HttpResponseRedirect(self.get_success_url())
+        else:
+            self.object.save()
+            return HttpResponseRedirect(self.get_success_url())
 
     def get_success_url(self):
         return reverse("ticket:ticket_detail", kwargs={"pk": self.object.pk})
