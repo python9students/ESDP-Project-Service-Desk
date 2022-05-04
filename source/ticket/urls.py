2ff57bb9fcc39b08414ba6e59ab6dfd81fc8e26e
ghost <kirill.han011@gmail.com>
2022-05-05 00:25:37 +0600

    № 46. Удалена возможность создания, удаления и обновления сервисного обслуживания. Эти функции доступны только в админ панели

--- source/ticket/urls.py
+++ source/ticket/urls.py
@@ -12,19 +12,13 @@ from ticket.views.clients import (ClientListView,
                                   ClientDeleteView,
                                   IndexView)
 from ticket.views.service_object import (ServiceObjectListView,
-                                         ServiceObjectCreateView,
-                                         ServiceObjectUpdateView,
-                                         ServiceObjectDetailView,
-                                         ServiceObjectDeleteView)
+                                         ServiceObjectDetailView)
 
 app_name = 'ticket'
 
 service_object_urlpatterns = [
     path('', ServiceObjectListView.as_view(), name='service_object_list'),
-    path('create/', ServiceObjectCreateView.as_view(), name='service_object_create'),
-    path('update/<int:pk>/', ServiceObjectUpdateView.as_view(), name='service_object_update'),
     path('<int:pk>/', ServiceObjectDetailView.as_view(), name='service_object_detail'),
-    path('delete/<int:pk>/', ServiceObjectDeleteView.as_view(), name='service_object_delete'),
 ]
 
 client_urlpatterns = [
--- source/ticket/views/service_object.py
+++ source/ticket/views/service_object.py
@@ -1,7 +1,6 @@
-from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, DeleteView
 from ticket.models import ServiceObject
-from ticket.forms import ServiceObjectForm
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse_lazy
 
 
 class ServiceObjectListView(ListView):
@@ -10,33 +9,8 @@ class ServiceObjectListView(ListView):
     context_object_name = 'service_objects'
 
 
-class ServiceObjectCreateView(CreateView):
-    model = ServiceObject
-    template_name = 'service_object/create.html'
-    form_class = ServiceObjectForm
-
-    def get_success_url(self):
-        return reverse("ticket:service_object_detail", kwargs={"pk": self.object.pk})
-
-
-class ServiceObjectUpdateView(UpdateView):
-    model = ServiceObject
-    template_name = 'service_object/update.html'
-    context_object_name = 'service_object'
-    form_class = ServiceObjectForm
-
-    def get_success_url(self):
-        return reverse("ticket:service_object_detail", kwargs={"pk": self.object.pk})
-
-
 class ServiceObjectDetailView(DetailView):
     model = ServiceObject
     template_name = 'service_object/detail.html'
     context_object_name = 'service_object'
 
-
-class ServiceObjectDeleteView(DeleteView):
-    model = ServiceObject
-    template_name = 'service_object/delete.html'
-    context_object_name = 'service_object'
-    success_url = reverse_lazy('ticket:service_object_list')
