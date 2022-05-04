7a9ade5e0847375b5e9eb7ec9d52a3e39e187d83
ghost <kirill.han011@gmail.com>
2022-05-05 00:26:17 +0600

    № 46. Удалена возможность создания, удаления и обновления сервисного обслуживания. Эти функции доступны только в админ панели

--- source/ticket/views/service_object.py
+++ source/ticket/views/service_object.py
@@ -1,6 +1,5 @@
-from django.views.generic import ListView, DetailView, DeleteView
+from django.views.generic import ListView, DetailView
 from ticket.models import ServiceObject
-from django.urls import reverse_lazy
 
 
 class ServiceObjectListView(ListView):
@@ -13,4 +12,3 @@ class ServiceObjectDetailView(DetailView):
     model = ServiceObject
     template_name = 'service_object/detail.html'
     context_object_name = 'service_object'
-
