ea3e564b883133fde1d78229fd4b2895b79084a5
nooruzbay <nooruzbay@gmail.com>
2022-05-12 18:22:14 +0600

    Вывод ошибок при логине #57

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,9 +1,11 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path
+
+# from accounts.views import MyLoginView
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name="login.html"), name="login"),
+    path('login/', LoginView.as_view(template_name='login.html'), name="login"),
     path('logout/', LogoutView.as_view(), name="logout"),
 ]
