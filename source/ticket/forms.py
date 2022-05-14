7e5e1365a6f776ea61b11124fb2f50790c82804f
farkhatzhan-makhmudov <faralost@gmail.com>
2022-05-14 16:18:16 +0600

    #83 у группы начальников теперь доступны инженерские поля для редактирования

--- source/ticket/forms.py
+++ source/ticket/forms.py
@@ -4,143 +4,32 @@ from django.utils import timezone
 
 from ticket.models import Work, ProblemArea, ServiceObject, Client, Ticket
 from django.core.exceptions import ValidationError
-from django.forms import TextInput, widgets
+from django.forms import TextInput, widgets, BaseModelForm
 from mptt.forms import TreeNodeMultipleChoiceField
 
 User = get_user_model()
 
 
-class ServiceObjectForm(forms.ModelForm):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if self.instance:
-            self.initial['active_from'] = self.instance.active_from.strftime(
-                '%Y-%m-%dT%H:%M') if self.instance.active_from else None
-
-    class Meta:
-        model = ServiceObject
-        fields = '__all__'
-        widgets = {
-            'active_from': forms.DateTimeInput(format='%d/%m/%Y %H:%M',
-                                               attrs={'type': 'datetime-local'}),
-        }
-
-
-class ClientForm(forms.ModelForm):
-    website = forms.URLField(widget=TextInput)
-
-    class Meta:
-        model = Client
-        fields = '__all__'
-
-
-class ChiefForm(forms.ModelForm):
-    description = forms.CharField(required=False, max_length=1000,
-                                  widget=widgets.Textarea(), label='Описание')
-    executor = forms.ModelChoiceField(queryset=User.objects.all(), required=False, label='Исполнитель')
-    driver = forms.ModelChoiceField(queryset=User.objects.all(), required=False, label='Водитель')
-    works = TreeNodeMultipleChoiceField(queryset=Work.objects.all(),
-                                        widget=widgets.SelectMultiple(attrs={'size': 20}),
-                                        label='Работы')
-    problem_areas = TreeNodeMultipleChoiceField(queryset=ProblemArea.objects.all(),
-                                                widget=widgets.SelectMultiple(attrs={'size': 20}),
-                                                label='Проблемные области')
-    desired_to = forms.DateTimeField(required=False, label='Желаемая дата исполнения',
-                                     widget=widgets.DateTimeInput(format='%d/%m/%Y %H:%M',
-                                                                  attrs={'type': 'datetime-local'}),
-                                     )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if self.instance:
-            self.initial['recieved_at'] = self.instance.recieved_at.strftime(
-                '%Y-%m-%dT%H:%M') if self.instance.recieved_at else None
-            self.initial['desired_to'] = self.instance.desired_to.strftime(
-                '%Y-%m-%dT%H:%M') if self.instance.desired_to else None
-            self.initial['closed_at'] = self.instance.closed_at.strftime(
-                '%Y-%m-%dT%H:%M') if self.instance.closed_at else None
-
-    class Meta:
-        model = Ticket
-        widgets = {
-            'recieved_at': forms.DateTimeInput(format='%d/%m/%Y %H:%M',
-                                               attrs={'type': 'datetime-local'}),
-            'closed_at': forms.DateTimeInput(format='%d/%m/%Y %H:%M',
-                                             attrs={'type': 'datetime-local'}),
-        }
-        exclude = ("work_started_at", "work_finished_at", "ride_started_at", "ride_finished_at", "cancel_reason",
-                   "closed_at", "operator", "status")
-
+class TicketFormValidationMixin(BaseModelForm):
     def clean(self):
         cleaned_data = super().clean()
         recieved_at = cleaned_data['recieved_at']
+        desired_to = cleaned_data['desired_to']
+        work_started_at = cleaned_data['work_started_at']
+        work_finished_at = cleaned_data['work_finished_at']
+        ride_started_at = cleaned_data['ride_started_at']
+        ride_finished_at = cleaned_data['ride_finished_at']
 
         if recieved_at and recieved_at > timezone.now():
             self.add_error('recieved_at',
                            ValidationError("Дата получения заявки не может быть позже чем время сейчас"))
             self.fields['recieved_at'].widget.attrs.update({'style': 'border-color:red;'})
-
-
-class EngineerForm(forms.ModelForm):
-    works = TreeNodeMultipleChoiceField(queryset=Work.objects.all(),
-                                        widget=widgets.SelectMultiple(attrs={'size': 20}),
-                                        label='Работы')
-    problem_areas = TreeNodeMultipleChoiceField(queryset=ProblemArea.objects.all(),
-                                                widget=widgets.SelectMultiple(attrs={'size': 20}),
-                                                label='Проблемные области')
-    work_started_at = forms.DateTimeField(required=False, label='Дата начала работ',
-                                          widget=widgets.DateTimeInput(format='%d/%m/%Y %H:%M',
-                                                                       attrs={'type': 'datetime-local'}),
-                                          )
-    work_finished_at = forms.DateTimeField(required=False, label='Дата окончания работ',
-                                           widget=widgets.DateTimeInput(format='%d/%m/%Y %H:%M',
-                                                                        attrs={'type': 'datetime-local'}),
-                                           )
-    ride_finished_at = forms.DateTimeField(required=False, label='Дата окончания поездки',
-                                           widget=widgets.DateTimeInput(format='%d/%m/%Y %H:%M',
-                                                                        attrs={'type': 'datetime-local'}),
-                                           )
-    closed_at = forms.DateTimeField(required=False, label='Дата закрытия заявки',
-                                    widget=widgets.DateTimeInput(format='%d/%m/%Y %H:%M',
-                                                                 attrs={'type': 'datetime-local'}),
-                                    )
-    desired_to = forms.DateTimeField(required=False, label='Желаемая дата исполнения',
-                                     widget=widgets.DateTimeInput(format='%d/%m/%Y %H:%M',
-                                                                  attrs={'type': 'datetime-local'}),
-                                     )
-
-    class Meta:
-        model = Ticket
-        exclude = ("cancel_reason", "executor", "driver")
-        widgets = {
-            'ride_started_at': forms.DateTimeInput(format='%d/%m/%Y %H:%M',
-                                                   attrs={'type': 'datetime-local'}),
-        }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        fields = ['client', 'service_object', 'priority', 'type', 'status', 'service_level', 'department',
-                  'recieved_at',
-                  'desired_to', 'operator', 'description', 'closed_at']
-        for field in fields:
-            self.fields[field].disabled = True
-        if self.instance:
-            self.initial['work_started_at'] = self.instance.work_started_at.strftime(
-                '%Y-%m-%dT%H:%M') if self.instance.work_started_at else None
-            self.initial['work_finished_at'] = self.instance.work_finished_at.strftime(
-                '%Y-%m-%dT%H:%M') if self.instance.work_finished_at else None
-            self.initial['ride_started_at'] = self.instance.ride_started_at.strftime(
-                '%Y-%m-%dT%H:%M') if self.instance.ride_started_at else None
-            self.initial['ride_finished_at'] = self.instance.ride_finished_at.strftime(
-                '%Y-%m-%dT%H:%M') if self.instance.ride_finished_at else None
-
-    def clean(self):
-        cleaned_data = super().clean()
-        work_started_at = cleaned_data['work_started_at']
-        work_finished_at = cleaned_data['work_finished_at']
-        ride_started_at = cleaned_data['ride_started_at']
-        ride_finished_at = cleaned_data['ride_finished_at']
+        if recieved_at and desired_to:
+            if recieved_at > desired_to:
+                self.add_error('desired_to',
+                               ValidationError(
+                                   "Желаемая дата исполнения не может быть позже чем дата получения заявки"))
+                self.fields['desired_to'].widget.attrs.update({'style': 'border-color:red;'})
 
         if ride_finished_at and ride_started_at:
             if ride_finished_at < ride_started_at:
@@ -173,6 +62,117 @@ class EngineerForm(forms.ModelForm):
                                ValidationError("Дата окончания поездки не может быть раньше начала работ"))
                 self.fields['ride_finished_at'].widget.attrs.update({'style': 'border-color:red;'})
         return cleaned_data
+
+
+class ChiefForm(forms.ModelForm, TicketFormValidationMixin):
+    description = forms.CharField(required=False, max_length=1000,
+                                  widget=widgets.Textarea(), label='Описание')
+    executor = forms.ModelChoiceField(queryset=User.objects.all(), required=False, label='Исполнитель')
+    driver = forms.ModelChoiceField(queryset=User.objects.all(), required=False, label='Водитель')
+    works = TreeNodeMultipleChoiceField(queryset=Work.objects.all(),
+                                        widget=widgets.SelectMultiple(attrs={'size': 20}),
+                                        label='Работы')
+    problem_areas = TreeNodeMultipleChoiceField(queryset=ProblemArea.objects.all(),
+                                                widget=widgets.SelectMultiple(attrs={'size': 20}),
+                                                label='Проблемные области')
+    desired_to = forms.DateTimeField(required=False, label='Желаемая дата исполнения',
+                                     widget=widgets.DateTimeInput(format='%d/%m/%Y %H:%M',
+                                                                  attrs={'type': 'datetime-local'}),
+                                     )
+    work_started_at = forms.DateTimeField(required=False, label='Дата начала работ',
+                                          widget=widgets.DateTimeInput(format='%d/%m/%Y %H:%M',
+                                                                       attrs={'type': 'datetime-local'}),
+                                          )
+    work_finished_at = forms.DateTimeField(required=False, label='Дата окончания работ',
+                                           widget=widgets.DateTimeInput(format='%d/%m/%Y %H:%M',
+                                                                        attrs={'type': 'datetime-local'}),
+                                           )
+    ride_finished_at = forms.DateTimeField(required=False, label='Дата окончания поездки',
+                                           widget=widgets.DateTimeInput(format='%d/%m/%Y %H:%M',
+                                                                        attrs={'type': 'datetime-local'}),
+                                           )
+    ride_started_at = forms.DateTimeField(required=False, label='Дата начала поездки',
+                                          widget=widgets.DateTimeInput(format='%d/%m/%Y %H:%M',
+                                                                       attrs={'type': 'datetime-local'}),
+                                          )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance:
+            self.initial['recieved_at'] = self.instance.recieved_at.strftime(
+                '%Y-%m-%dT%H:%M') if self.instance.recieved_at else None
+            self.initial['desired_to'] = self.instance.desired_to.strftime(
+                '%Y-%m-%dT%H:%M') if self.instance.desired_to else None
+            self.initial['closed_at'] = self.instance.closed_at.strftime(
+                '%Y-%m-%dT%H:%M') if self.instance.closed_at else None
+            self.initial['work_started_at'] = self.instance.work_started_at.strftime(
+                '%Y-%m-%dT%H:%M') if self.instance.work_started_at else None
+            self.initial['work_finished_at'] = self.instance.work_finished_at.strftime(
+                '%Y-%m-%dT%H:%M') if self.instance.work_finished_at else None
+            self.initial['ride_started_at'] = self.instance.ride_started_at.strftime(
+                '%Y-%m-%dT%H:%M') if self.instance.ride_started_at else None
+            self.initial['ride_finished_at'] = self.instance.ride_finished_at.strftime(
+                '%Y-%m-%dT%H:%M') if self.instance.ride_finished_at else None
+
+    class Meta:
+        model = Ticket
+        widgets = {
+            'recieved_at': forms.DateTimeInput(format='%d/%m/%Y %H:%M',
+                                               attrs={'type': 'datetime-local'}),
+            'closed_at': forms.DateTimeInput(format='%d/%m/%Y %H:%M',
+                                             attrs={'type': 'datetime-local'}),
+        }
+        exclude = ("cancel_reason", "closed_at", "operator", "status")
+
+
+class EngineerForm(forms.ModelForm, TicketFormValidationMixin):
+    works = TreeNodeMultipleChoiceField(queryset=Work.objects.all(),
+                                        widget=widgets.SelectMultiple(attrs={'size': 20}),
+                                        label='Работы')
+    problem_areas = TreeNodeMultipleChoiceField(queryset=ProblemArea.objects.all(),
+                                                widget=widgets.SelectMultiple(attrs={'size': 20}),
+                                                label='Проблемные области')
+    work_started_at = forms.DateTimeField(required=False, label='Дата начала работ',
+                                          widget=widgets.DateTimeInput(format='%d/%m/%Y %H:%M',
+                                                                       attrs={'type': 'datetime-local'}),
+                                          )
+    work_finished_at = forms.DateTimeField(required=False, label='Дата окончания работ',
+                                           widget=widgets.DateTimeInput(format='%d/%m/%Y %H:%M',
+                                                                        attrs={'type': 'datetime-local'}),
+                                           )
+    ride_finished_at = forms.DateTimeField(required=False, label='Дата окончания поездки',
+                                           widget=widgets.DateTimeInput(format='%d/%m/%Y %H:%M',
+                                                                        attrs={'type': 'datetime-local'}),
+                                           )
+    desired_to = forms.DateTimeField(required=False, label='Желаемая дата исполнения',
+                                     widget=widgets.DateTimeInput(format='%d/%m/%Y %H:%M',
+                                                                  attrs={'type': 'datetime-local'}),
+                                     )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        fields = ['client', 'service_object', 'priority', 'type', 'status', 'service_level', 'department',
+                  'recieved_at', 'desired_to', 'operator', 'description', 'closed_at']
+        for field in fields:
+            self.fields[field].disabled = True
+
+        if self.instance:
+            self.initial['work_started_at'] = self.instance.work_started_at.strftime(
+                '%Y-%m-%dT%H:%M') if self.instance.work_started_at else None
+            self.initial['work_finished_at'] = self.instance.work_finished_at.strftime(
+                '%Y-%m-%dT%H:%M') if self.instance.work_finished_at else None
+            self.initial['ride_started_at'] = self.instance.ride_started_at.strftime(
+                '%Y-%m-%dT%H:%M') if self.instance.ride_started_at else None
+            self.initial['ride_finished_at'] = self.instance.ride_finished_at.strftime(
+                '%Y-%m-%dT%H:%M') if self.instance.ride_finished_at else None
+
+    class Meta:
+        model = Ticket
+        exclude = ("cancel_reason", "executor", "driver")
+        widgets = {
+            'ride_started_at': forms.DateTimeInput(format='%d/%m/%Y %H:%M',
+                                                   attrs={'type': 'datetime-local'}),
+        }
 
 
 class TicketCancelForm(forms.ModelForm):
