85059462971b875df11d165831af1fd44901af6d
farkhatzhan-makhmudov <faralost@gmail.com>
2022-06-03 17:39:30 +0600

    #121 исправил селекты в фильтрации запчастей на бутсраповские

--- source/ticket/filters.py
+++ source/ticket/filters.py
@@ -3,7 +3,7 @@ from django.forms import DateInput, Select
 from django_filters import DateFilter, ModelChoiceFilter
 
 from ticket.models.client import Client
-from ticket.models.spare_part import SparePartUser
+from ticket.models.spare_part import SparePartUser, SparePart
 from ticket.models.ticket import Ticket, User, TicketStatus
 
 
@@ -39,11 +39,23 @@ class TicketFilter(django_filters.FilterSet):
 
 
 class SparePartUserFilter(django_filters.FilterSet):
-    engineer = ModelChoiceFilter(queryset=User.objects.all(), label='Назначена кому')
+    spare_part = django_filters.ModelChoiceFilter(label='Запчасть', queryset=SparePart.objects.all(),
+                                                  widget=Select(attrs={'class': 'form-select form-select-sm',
+                                                                       'style': "width:220px; display:inline-flex;"}))
+    assigned_by = django_filters.ModelChoiceFilter(label='Назначена кем', queryset=User.objects.all(),
+                                                   widget=Select(attrs={'class': 'form-select form-select-sm',
+                                                                        'style': "width:170px; display:inline-flex;"}))
+    engineer = ModelChoiceFilter(queryset=User.objects.all(), label='Назначена кому',
+                                 widget=Select(attrs={'class': 'form-select form-select-sm',
+                                                      'style': "width:170px; display:inline-flex;"})
+                                 )
+    status = django_filters.ChoiceFilter(label='Статус', choices=SparePartUser.SPARE_PART_STATUS_CHOICES,
+                                         widget=Select(attrs={'class': 'form-select form-select-sm',
+                                                              'style': "width:150px; display:inline-flex;"}))
 
     class Meta:
         model = SparePartUser
-        fields = ['assigned_by', 'engineer', 'status']
+        fields = ['spare_part', 'assigned_by', 'engineer', 'status']
 
     def __init__(self, *args, **kwargs):
         super(SparePartUserFilter, self).__init__(*args, **kwargs)
